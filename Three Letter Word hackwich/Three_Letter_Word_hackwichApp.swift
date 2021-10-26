dbbc837468fc1c6618140baf6024a67e29e1a419
Jack <Jwenschhof@bsd220.org>
2021-10-26 09:55:31 -0500

    Initial Commit

---
+++ Three Letter Word hackwich/Three_Letter_Word_hackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_Word_hackwichApp.swift
+//  Three Letter Word hackwich
+//
+//  Created by Student on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_Word_hackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
